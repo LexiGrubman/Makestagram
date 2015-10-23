f4c5f71a2ce8d7736db807cbbfe53f48cbce7249
Alexis <alexisgrubman@Alexiss-MacBook-Pro.local>
2015-10-23 11:54:42 -0700

    Added tab buttons w/limited functionality

---
+++ Makestagram/PhotoTakingHelper.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoTakingHelper.swift
+//  Makestagram
+//
+//  Created by Alexis Grubman on 10/23/15.
+//  Copyright © 2015 Make School. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTakingHelper: NSObject {
+
+}
