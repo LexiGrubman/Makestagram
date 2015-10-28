89221cddc6d81e126b1d339b61182148bd6d4e9b
Alexis <alexisgrubman@Alexiss-MacBook-Pro.local>
2015-10-28 16:35:14 -0700

    Timeline cells added

---
+++ Makestagram/PostTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PostTableViewCell.swift
+//  Makestagram
+//
+//  Created by Alexis Grubman on 10/28/15.
+//  Copyright © 2015 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+    //...
+    @IBOutlet var postImageView: UIImageView!
+}
--- Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -11,11 +11,17 @@ import Parse
 
 class TimelineViewController: UIViewController {
     
+
+    @IBOutlet var tableView: UITableView!
+    
     var photoTakingHelper: PhotoTakingHelper?
+    
+    var posts: [Post] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
         self.tabBarController?.delegate = self
     }
     
@@ -25,13 +31,43 @@ class TimelineViewController: UIViewController {
         photoTakingHelper =
             PhotoTakingHelper(viewController: self.tabBarController!) { (image: UIImage?) in
                 let post = Post()
-                post.image = image
+                post.image.value = image
                 post.uploadPost()
         }
     }
     
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // 1
+        let followingQuery = PFQuery(className: "Follow")
+        followingQuery.whereKey("fromUser", equalTo:PFUser.currentUser()!)
+        
+        // 2
+        let postsFromFollowedUsers = Post.query()
+        postsFromFollowedUsers!.whereKey("user", matchesKey: "toUser", inQuery: followingQuery)
+        
+        // 3
+        let postsFromThisUser = Post.query()
+        postsFromThisUser!.whereKey("user", equalTo: PFUser.currentUser()!)
+        
+        // 4
+        let query = PFQuery.orQueryWithSubqueries([postsFromFollowedUsers!, postsFromThisUser!])
+        // 5
+        query.includeKey("user")
+        // 6
+        query.orderByDescending("createdAt")
+        
+        // 7
+        query.findObjectsInBackgroundWithBlock {(result: [AnyObject]?, error: NSError?) -> Void in
+            // 8
+            self.posts = result as? [Post] ?? []
+            // 9
+            self.tableView.reloadData()
+        }
+    }
+    
 }
-
 // MARK: Tab Bar Delegate
 
 
@@ -46,4 +82,21 @@ extension TimelineViewController: UITabBarControllerDelegate {
         }
     }
 
+}
+    extension TimelineViewController: UITableViewDataSource {
+        
+        func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            // 1
+            return posts.count
+        }
+        
+        func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            // 2
+            let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostTableViewCell
+            
+            cell.postImageView.image = posts[indexPath.row].image.value
+            
+            return cell
+        }
+        
 }
